a950548db323ccdb7e167cb73e1c3d39a07920ed
ruslanjava <ruslan.java@gmail.com>
2020-01-27 01:08:24 +0300

    Код дополнен по 4 лекции (33 минута видео)

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/Bottombar.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/Bottombar.kt
@@ -1,11 +1,15 @@
 package ru.skillbranch.skillarticles.ui.custom
 
 import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.isVisible
 import com.google.android.material.shape.MaterialShapeDrawable
+import kotlinx.android.synthetic.main.layout_bottombar.view.*
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.ui.custom.behaviors.BottomBarBehavior
 
@@ -14,6 +18,8 @@ class Bottombar @JvmOverloads constructor(
         attrs: AttributeSet? = null,
         defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr), CoordinatorLayout.AttachedBehavior {
+
+    var isSearchMode = false
 
     init {
         View.inflate(context, R.layout.layout_bottombar, this)
@@ -24,6 +30,53 @@ class Bottombar @JvmOverloads constructor(
 
     override fun getBehavior(): CoordinatorLayout.Behavior<*> {
         return BottomBarBehavior()
+    }
+
+    // save state
+    override fun onSaveInstanceState(): Parcelable? {
+        val savedState = SavedState(super.onSaveInstanceState())
+        savedState.ssIsSearchMode = isSearchMode
+        return savedState
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable) {
+        super.onRestoreInstanceState(state)
+        if (state is SavedState) {
+            isSearchMode = state.ssIsSearchMode
+            reveal.isVisible = !isSearchMode
+            group_bottom.isVisible = !isSearchMode
+        }
+    }
+
+    private class SavedState: BaseSavedState, Parcelable {
+
+        var ssIsSearchMode: Boolean = false
+
+        constructor(superState: Parcelable?) : super(superState)
+
+        constructor(src: Parcel) : super(src) {
+            ssIsSearchMode = src.readInt() == 1
+        }
+
+        override fun writeToParcel(out: Parcel, flags: Int) {
+            super.writeToParcel(out, flags)
+            out.writeInt(if (ssIsSearchMode) 1 else 0)
+        }
+
+        override fun describeContents(): Int = 0
+
+        companion object CREATOR : Parcelable.Creator<SavedState> {
+
+            override fun createFromParcel(source: Parcel): SavedState {
+                return SavedState(source)
+            }
+
+            override fun newArray(size: Int): Array<SavedState?> {
+                return arrayOfNulls(size)
+            }
+
+        }
+
     }
 
 }
