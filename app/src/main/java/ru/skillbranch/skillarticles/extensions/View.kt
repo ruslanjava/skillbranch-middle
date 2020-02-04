8f7ea6332245038ec0a7fbe14690ed41a37648d0
ruslanjava <ruslan.java@gmail.com>
2020-02-05 01:16:21 +0300

    Дополнительные правки дерева проекта

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
@@ -1,9 +1,9 @@
 package ru.skillbranch.skillarticles.extensions
 
+import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.NestedScrollView
 
-fun NestedScrollView.setMarginOptionally(top: Int? = null, bottom: Int? = null, start: Int? = null, end : Int? = null) {
+fun View.setMarginOptionally(top: Int? = null, bottom: Int? = null, start: Int? = null, end : Int? = null) {
     val layoutParams = this.layoutParams as ViewGroup.LayoutParams
     if (layoutParams is ViewGroup.MarginLayoutParams) {
         top?.let { layoutParams.topMargin = it }
