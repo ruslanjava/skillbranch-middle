3ea0f7eebc4bfc70d73a64dec0a4cd5eb2202ae6
ruslanjava <ruslan.java@gmail.com>
2020-02-08 18:21:43 +0300

    Убраны методы get/set, чтобы избежать ошибки "Unresolved reference"

--- app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefDelegate.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefDelegate.kt
@@ -10,22 +10,37 @@ class PrefDelegate<T>(private val defaultValue: T) : ReadWriteProperty<PrefManag
     @Suppress("UNCHECKED_CAST")
     override fun getValue(thisRef: PrefManager, property: KProperty<*>): T? {
         return when (defaultValue) {
-            is Boolean -> thisRef.getBoolean(property.name, defaultValue as Boolean) as T
-            is String -> thisRef.getString(property.name, defaultValue as String) as T
-            is Float -> thisRef.getFloat(property.name, defaultValue as Float) as T
-            is Int -> thisRef.getInt(property.name, defaultValue as Int) as T
-            is Long -> thisRef.getLong(property.name, defaultValue as Long) as T
+            is Boolean -> thisRef.preferences.getBoolean(property.name, defaultValue as Boolean) as T
+            is String -> thisRef.preferences.getString(property.name, defaultValue as String) as T
+            is Float -> thisRef.preferences.getFloat(property.name, defaultValue as Float) as T
+            is Int -> thisRef.preferences.getInt(property.name, defaultValue as Int) as T
+            is Long -> thisRef.preferences.getLong(property.name, defaultValue as Long) as T
             else -> throw IllegalArgumentException("Unsupported value: $defaultValue")
         }
     }
 
     override fun setValue(thisRef: PrefManager, property: KProperty<*>, value: T?) {
         when (defaultValue) {
-            is Boolean -> thisRef.setBoolean(property.name, value as Boolean, defaultValue)
-            is String -> thisRef.setString(property.name, value as String, defaultValue)
-            is Float -> thisRef.setFloat(property.name, value as Float, defaultValue)
-            is Long -> thisRef.setLong(property.name, value as Long, defaultValue)
-            is Int -> thisRef.setInt(property.name, value as Int, defaultValue)
+            is Boolean -> {
+                val savedValue = value ?: defaultValue
+                thisRef.preferences.edit().putBoolean(property.name, savedValue as Boolean).apply()
+            }
+            is String -> {
+                val savedValue: String = (value ?: defaultValue) as String
+                thisRef.preferences.edit().putString(property.name, savedValue).apply()
+            }
+            is Float -> {
+                val savedValue: Float = (value ?: defaultValue) as Float
+                thisRef.preferences.edit().putFloat(property.name, savedValue).apply()
+            }
+            is Long -> {
+                val savedValue: Long = (value ?: defaultValue) as Long
+                thisRef.preferences.edit().putLong(property.name, savedValue).apply()
+            }
+            is Int -> {
+                val savedValue: Int = (value ?: defaultValue) as Int
+                thisRef.preferences.edit().putInt(property.name, savedValue).apply()
+            }
             else -> throw IllegalArgumentException("Unsupported value: $defaultValue")
         }
     }
