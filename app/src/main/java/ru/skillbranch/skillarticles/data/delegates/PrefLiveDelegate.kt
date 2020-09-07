ef09e64643b2b6fbd745073199faf5920d9f65b1
ruslanjava <ruslan.java@gmail.com>
2020-09-08 00:29:48 +0300

    Перенос PrefLiveObjDelegate

--- app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefLiveDelegate.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefLiveDelegate.kt
@@ -2,6 +2,8 @@ package ru.skillbranch.skillarticles.data.delegates
 
 import android.content.SharedPreferences
 import androidx.lifecycle.LiveData
+import com.squareup.moshi.JsonAdapter
+import ru.skillbranch.skillarticles.data.local.PrefManager
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
@@ -55,6 +57,52 @@ internal class SharedPreferenceLiveData<T>(
             is Boolean -> sharedPrefs.getBoolean(key, defaultValue as Boolean) as T
             else -> error("This type $defaultValue can't be stored int Preferences")
         }
+    }
+
+}
+
+class PrefLiveObjDelegate<T>(
+    private val fieldKey: String,
+    private val adapter: JsonAdapter<T>,
+    private val preferences: SharedPreferences) :
+    ReadOnlyProperty<PrefManager, LiveData<T?>> {
+
+    private var storedValue: LiveData<T?>? = null
+
+    override fun getValue(thisRef: PrefManager, property: KProperty<*>): LiveData<T?> {
+        if (storedValue == null) {
+            storedValue = SharedPreferencesLiveObjData(preferences, fieldKey, adapter)
+        }
+        return storedValue!!
+    }
+
+}
+
+internal class SharedPreferencesLiveObjData<T>(
+    private val sharedPrefs: SharedPreferences,
+    var key: String,
+    var adapter: JsonAdapter<T>
+): LiveData<T?>() {
+
+    private val preferencesChangeListener = SharedPreferences.OnSharedPreferenceChangeListener { _, shKey ->
+        if (shKey == key) {
+            value = readValue()
+        }
+    }
+
+    override fun onActive() {
+        super.onActive()
+        value = readValue()
+        sharedPrefs.registerOnSharedPreferenceChangeListener(preferencesChangeListener)
+    }
+
+    override fun onInactive() {
+        sharedPrefs.unregisterOnSharedPreferenceChangeListener(preferencesChangeListener)
+        super.onInactive()
+    }
+
+    private fun readValue(): T? {
+        return sharedPrefs.getString(key, null)?.let { adapter.fromJson(it) }
     }
 
 }
